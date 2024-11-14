13f651b8a373c4aed7f973feb7e26e7815b9164e
Sandy-op <sandeep.cnct404@gmail.com>
2024-11-14 19:37:08 +0530

    add reserve seatBySeatNo. functionality and change whole front-end

--- Bus-Reservation-App-Master/reservation-fe/src/App.jsx
+++ Bus-Reservation-App-Master/reservation-fe/src/App.jsx
@@ -1,50 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route,Routes } from 'react-router-dom';
-import LandingPage from './Components/LandingPage';
-import AdminLogin from './Components/Admin/AdminLogin';
-import AdminSignUp from './Components/Admin/AdminSignUp';
-import AdminHomePage from './Components/Admin/AdminHomePage';
-import PageNotFound from './Components/PageNotFound';
-import Protect from './Components/Protect';
-import UserSignUp from './Components/User/UserSignUp';
-import UserLogin from './Components/User/UserLogin';
-import UserForgotPassword from './Components/User/UserForgotPassword';
-import UserResetPassword from './Components/User/UserResetPassword';
-import ResetConfirmation from './Components/ResetConfirmation';
-import BookBus from './Components/Bus/BookBus';
-import SearchBus from './Components/Bus/SearchBus';
-import BookTicket from './Components/Bus/BookTicket';
-import { AuthProvider } from './Components/AuthContext';
-import AdminForgotPassword from './Components/Admin/AdminForgotPassword';
-import AdminResetPassword from './Components/Admin/AdminResetPassword';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PageNotFound from './components/PageNotFound';
+import NavBar from './components/NavBar/NavBar'
+import Footer from './components/Footer/Footer';
+import LandingPage from './pages/LandingPage';
+import Bus from './pages/bus/Bus';
+import Detail from './pages/bus/Detail';
+import Checkout from './pages/checkout/Checkout';
 
 
 function App() {
   return (
-    <div className="App">
-      <AuthProvider>
-     <BrowserRouter>
-     <Routes>
-      <Route path='/' element={<LandingPage/>}/>
-      <Route path='/adminlogin' element={<AdminLogin/>}/>
-      <Route path='/usersignup' element={<UserSignUp/>}/>
-      <Route path='/userlogin' element={<UserLogin/>}/> 
-      <Route path='/adminsignup' element={<AdminSignUp/>}/>
-      <Route path='/bookbus/:id' element={<Protect Child={BookBus} role="user"/>}/>
-      <Route path='/adminhomepage/*' element={<Protect Child={AdminHomePage} role="admin" />}/>
-      <Route path='/user-forgot-password' element={<UserForgotPassword/>}/>
-      <Route path='/admin-forgot-password' element={<AdminForgotPassword/>} />
-      <Route path='/reset-confirmation' element={<ResetConfirmation />} />
-      <Route path='/user-reset-password' element={<UserResetPassword/>} />
-      <Route path='/admin-reset-password' element={<AdminResetPassword/>} />
-      <Route path='/search-bus' element={<SearchBus/>}/>
-      <Route path='/book-ticket/:busId/:userId/:numberOfSeats' element={<BookTicket/>} />
-      
-      <Route path='/*' element={<PageNotFound/>}/>
-     </Routes>
-     </BrowserRouter>
-     </AuthProvider>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <div className='w-full min-h-screen bg-neutral-50 dark:bg-neutral-950 text-neutral-800 dark:text-neutral-300 flex flex-col overflow-hidden'>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/bus' element={<Bus />} />
+          <Route path='bus/bus-details' element={<Detail />} />
+          <Route path='bus/bus-details/checkout' element={<Checkout />} />
+          <Route path='/*' element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
